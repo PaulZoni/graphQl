ba3f047e2a28d4f3bcfb39a4e7230a3457688a35
pavel <polykovpavel23@gmail.com>
2019-06-21 17:59:24 +0300

    + collapsing told bar

--- App.js
+++ App.js
@@ -7,14 +7,16 @@
  */
 
 import React, {Component, PureComponent} from 'react';
-import {StyleSheet, Text, View, FlatList, NativeModules, Platform, DeviceEventEmitter,
-    requireNativeComponent, NativeEventEmitter} from 'react-native';
+import {StyleSheet, Text, View, FlatList, NativeModules,
+    Platform, DeviceEventEmitter, Animated, TouchableOpacity, Easing, ScrollView, Dimensions,
+    requireNativeComponent, NativeEventEmitter,} from 'react-native';
 import NetworkManager from "./src/netwok/NetworkManager";
 import QueryPost from "./src/netwok/query/QueryPost";
 import MapView from "./src/view/MapView";
 var AndroidButton = requireNativeComponent('RCTButton');
 
-
+const HEADER_EXPANDED_HEIGHT = 300;
+const HEADER_COLLAPSED_HEIGHT = 60;
 
 
 export default class App extends Component<Props> {
@@ -22,8 +24,25 @@ export default class App extends Component<Props> {
     constructor(props) {
         super(props);
         this.state = {
-            allPosts: []
+            allPosts: [],
+            fadeAnim: new Animated.Value(1),
+            spinnerAnim: new Animated.Value(0),
+            springAnim: new Animated.Value(0),
+            springMovingMargin: new Animated.Value(0),
+            scrollY: new Animated.Value(0)
+
         };
+        this.buttonClick = false;
+        this.spinner = this.state.spinnerAnim.interpolate({
+            inputRange:[0,1],
+            outputRange: ['0deg', '720deg'],
+        });
+
+        this.movingMargin = this.state.springMovingMargin.interpolate({
+            inputRange: [0, 0.5, 1],
+            outputRange: [0, 300, 0]
+        });
+
         this._iosNativeEvent();
     }
 
@@ -32,6 +51,7 @@ export default class App extends Component<Props> {
         this._androidNativeEvent();
         this._iosModule();
     }
+
 
     _iosNativeEvent() {
         if (Platform.OS === 'ios') {
@@ -59,20 +79,145 @@ export default class App extends Component<Props> {
         }
     }
 
+    _buttonUp() {
+        Animated.parallel([
+            Animated.timing(this.state.fadeAnim, {
+                toValue: 1,
+                duration: 2000,
+                easing: Easing.back()
+            }),
+
+            Animated.timing(this.state.spinnerAnim, {
+                toValue: 0,
+                duration: 2000,
+                easing: Easing.linear
+            }),
+
+            Animated.spring(this.state.springAnim, {
+                toValue: 0,
+            })
+
+        ]).start();
+
+    }
+
+    _buttonDown() {
+        Animated.parallel([
+            Animated.timing(this.state.fadeAnim, {
+                toValue: 0.2,
+                duration: 2000,
+                easing: Easing.back()
+            }),
+            Animated.timing(this.state.spinnerAnim, {
+                toValue: 1,
+                duration: 2000,
+                easing: Easing.linear
+            }),
+
+            Animated.spring(this.state.springAnim, {
+                toValue: 1,
+                friction: 1
+            }),
+
+            Animated.spring(this.state.springMovingMargin, {
+                toValue: 1,
+                duration: 2000,
+                easing: Easing.linear
+            })
+
+
+        ]).start();
+
+    }
+
+    _spinerView() {
+        return(
+          <Animated.View
+            style={{
+                transform: [
+                        {rotate: this.spinner}, {scale: this.state.springAnim},
+
+                    ],
+                width: 50, height: 50, backgroundColor: '#883997', marginBottom: this.movingMargin
+            }}
+          />
+        );
+    }
+
+    _animatedView() {
+        return (
+            <View>
+                {this._spinerView()}
+                <TouchableOpacity
+                    onPress={() => {
+                        if (this.buttonClick) {
+                            this._buttonUp();
+                            this.buttonClick = false;
+                        } else {
+                            this._buttonDown();
+                            this.buttonClick = true;
+                        }
+                    }}
+                    style={{width: 200, height: 50, marginBottom: 20}}>
+                    <Animated.View style={{
+                        flex: 1,
+                        opacity: this.state.fadeAnim,
+                        backgroundColor: '#ba68c8',
+                    }}>
+
+                    </Animated.View>
+
+                </TouchableOpacity>
+            </View>
+
+        )
+    }
+
     render() {
+        const { width: SCREEN_WIDTH } = Dimensions.get('screen');
+
+        const headerHeight = this.state.scrollY.interpolate({
+            inputRange: [0, HEADER_EXPANDED_HEIGHT-HEADER_COLLAPSED_HEIGHT],
+            outputRange: [HEADER_EXPANDED_HEIGHT, HEADER_COLLAPSED_HEIGHT],
+            extrapolate: 'clamp'
+        });
+
         return (
             <View style={styles.container}>
+                <View style={{
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    zIndex: 3,
+                    backgroundColor: '#65499c',
+                    height: HEADER_COLLAPSED_HEIGHT,
+                    width: '100%'
+                }}/>
+                <Animated.View style={{
+                    height: headerHeight, backgroundColor: '#65499c',
+                    width: SCREEN_WIDTH, position: 'absolute', top: 0, left: 0, zIndex: 1
+                }}/>
+
                 <FlatList
+                    contentContainerStyle={{padding: 16, paddingTop: HEADER_EXPANDED_HEIGHT}}
+                    ListFooterComponent={this._animatedView() }
+                    style={{width: '100%', height: '100%', zIndex: 1, position: 'absolute'}}
                     keyExtractor={(item, index) => item.id}
                     data={this.state.allPosts}
+                    scrollEventThrottle={16}
+                    onScroll={Animated.event([{
+                        nativeEvent: {
+                            contentOffset: {y: this.state.scrollY}
+                        }
+                    }])
+                    }
                     renderItem={(item) =>
-                        <Text style={{padding: 10}}>
+                        <Text style={{padding: 10,}}>
                             {item.item.title}
                         </Text>
                     }
                 />
-                {this._mapIos()}
-                {this._androidButton()}
+
             </View>
         );
     }
@@ -102,7 +247,7 @@ export default class App extends Component<Props> {
 
     _requestPost() {
         let manager: NetworkManager = new NetworkManager('https://api.graph.cool/simple/v1/ciyz901en4j590185wkmexyex');
-        manager.executeQuery(new QueryPost(2),
+        manager.executeQuery(new QueryPost(15),
             (result) => this.setState({allPosts: result.data.allPosts}),
             (error) =>  console.log(JSON.stringify(error))
         );
@@ -114,8 +259,6 @@ const styles = StyleSheet.create({
 
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
         backgroundColor: '#F5FCFF',
     },
 
